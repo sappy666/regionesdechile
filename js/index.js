a3ae23bad329612feacd35a574ef9d8fce1c2076
Sappy <macarenaramdohr@gmail.com>
2023-03-03 12:26:25 -0300

    init repo

---
+++ js/index.js
@@ -0,0 +1,143 @@
+const r15 = document.getElementById('arica');
+const tarjetaArica = document.getElementById('tarjeta-arica');
+r15.addEventListener('mouseenter', function(){
+    tarjetaArica.style.display = 'flex';
+});
+r15.addEventListener('mouseleave', function(){
+    tarjetaArica.style.display = 'none';
+});
+
+const r2 = document.getElementById('antofagasta');
+const tarjetaAntofagasta = document.getElementById('tarjeta-antofagasta');
+r2.addEventListener('mouseenter', function(){
+    tarjetaAntofagasta.style.display = 'flex';
+});
+r2.addEventListener('mouseleave', function(){
+    tarjetaAntofagasta.style.display = 'none';
+});
+
+const r1 = document.getElementById('tarapaca');
+const tarjetaTarapaca = document.getElementById('tarjeta-tarapaca');
+r1.addEventListener('mouseenter', function(){
+    tarjetaTarapaca.style.display = 'flex';
+});
+r1.addEventListener('mouseleave', function(){
+    tarjetaTarapaca.style.display = 'none';
+});
+
+const r3 = document.getElementById('atacama');
+const tarjetaAtacama = document.getElementById('tarjeta-atacama');
+r3.addEventListener('mouseenter', function(){
+    tarjetaAtacama.style.display = 'flex';
+});
+r3.addEventListener('mouseleave', function(){
+    tarjetaAtacama.style.display = 'none';
+});
+
+const r4 = document.getElementById('coquimbo');
+const tarjetaCoquimbo = document.getElementById('tarjeta-coquimbo');
+r4.addEventListener('mouseenter', function(){
+    tarjetaCoquimbo.style.display = 'flex';
+});
+r4.addEventListener('mouseleave', function(){
+    tarjetaCoquimbo.style.display = 'none';
+});
+
+const r5 = document.getElementById('valparaiso');
+const tarjetaValparaiso = document.getElementById('tarjeta-valparaiso');
+r5.addEventListener('mouseenter', function(){
+    tarjetaValparaiso.style.display = 'flex';
+});
+r5.addEventListener('mouseleave', function(){
+    tarjetaValparaiso.style.display = 'none';
+});
+
+const r13 = document.getElementById('metropolitana');
+const tarjetaMetropolitana = document.getElementById('tarjeta-metropolitana');
+r13.addEventListener('mouseenter', function(){
+    tarjetaMetropolitana.style.display = 'flex';
+});
+r13.addEventListener('mouseleave', function(){
+    tarjetaMetropolitana.style.display = 'none';
+});
+
+const r6 = document.getElementById('ohiggins');
+const tarjetaOhiggins = document.getElementById('tarjeta-ohiggins');
+r6.addEventListener('mouseenter', function(){
+    tarjetaOhiggins.style.display = 'flex';
+});
+r6.addEventListener('mouseleave', function(){
+    tarjetaOhiggins.style.display = 'none';
+});
+
+const r7 = document.getElementById('maule');
+const tarjetaMaule = document.getElementById('tarjeta-maule');
+r7.addEventListener('mouseenter', function(){
+    tarjetaMaule.style.display = 'flex';
+});
+r7.addEventListener('mouseleave', function(){
+    tarjetaMaule.style.display = 'none';
+});
+
+const r8 = document.getElementById('biobio');
+const tarjetaBiobio = document.getElementById('tarjeta-biobio');
+r8.addEventListener('mouseenter', function(){
+    tarjetaBiobio.style.display = 'flex';
+});
+r8.addEventListener('mouseleave', function(){
+    tarjetaBiobio.style.display = 'none';
+});
+
+const r9 = document.getElementById('araucania');
+const tarjetaAraucania = document.getElementById('tarjeta-araucania');
+r9.addEventListener('mouseenter', function(){
+    tarjetaAraucania.style.display = 'flex';
+});
+r9.addEventListener('mouseleave', function(){
+    tarjetaAraucania.style.display = 'none';
+});
+
+const r14 = document.getElementById('rios');
+const tarjetaRios = document.getElementById('tarjeta-rios');
+r14.addEventListener('mouseenter', function(){
+    tarjetaRios.style.display = 'flex';
+});
+r14.addEventListener('mouseleave', function(){
+    tarjetaRios.style.display = 'none';
+});
+
+const r10 = document.getElementById('lagos');
+const tarjetaLagos = document.getElementById('tarjeta-lagos');
+r10.addEventListener('mouseenter', function(){
+    tarjetaLagos.style.display = 'flex';
+});
+r10.addEventListener('mouseleave', function(){
+    tarjetaLagos.style.display = 'none';
+});
+
+const r11 = document.getElementById('aisen');
+const tarjetaAisen = document.getElementById('tarjeta-aisen');
+r11.addEventListener('mouseenter', function(){
+    tarjetaAisen.style.display = 'flex';
+});
+r11.addEventListener('mouseleave', function(){
+    tarjetaAisen.style.display = 'none';
+});
+
+const r12 = document.getElementById('magallanes');
+const tarjetaMagallanes = document.getElementById('tarjeta-magallanes');
+r12.addEventListener('mouseenter', function(){
+    tarjetaMagallanes.style.display = 'flex';
+});
+r12.addEventListener('mouseleave', function(){
+    tarjetaMagallanes.style.display = 'none';
+});
+
+const r16 = document.getElementById('nuble');
+const tarjetaNuble = document.getElementById('tarjeta-nuble');
+r16.addEventListener('mouseenter', function(){
+    tarjetaNuble.style.display = 'flex';
+});
+r16.addEventListener('mouseleave', function(){
+    tarjetaNuble.style.display = 'none';
+});
