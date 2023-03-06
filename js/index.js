2997a229b18287ddf60eada487bb5b0d974a43a1
Sappy <macarenaramdohr@gmail.com>
2023-03-06 00:08:44 -0300

    css y js

--- js/index.js
+++ js/index.js
@@ -1,143 +1,134 @@
-const r15 = document.getElementById('arica');
-const tarjetaArica = document.getElementById('tarjeta-arica');
-r15.addEventListener('mouseenter', function(){
-    tarjetaArica.style.display = 'flex';
-});
-r15.addEventListener('mouseleave', function(){
-    tarjetaArica.style.display = 'none';
+var regionesDivs = document.querySelectorAll(".region");
+const carta = document.getElementById('carta-inf');
+const btnSubmit = document.getElementById('btn-submit');
+const btnCerrarForm = document.getElementById('btn-cerrar-form');
+const formActualizar = document.getElementById('form-modificar');
+
+const descripcion= document.getElementById('descripcion');
+const imagen= document.getElementById('imagen');
+
+let r1 =  {
+  nombre: 'Arica',
+  descripcion: "Capital regional: Arica",
+  imagen:"./assets/img/arica.jpg"
+};
+let r2 = {
+  nombre:"Tarapacá",
+  descripcion:"Capital regional: Iquique",
+  imagen:"./assets/img/tarapaca.jpg"
+};
+let r3 = {
+  nombre:"Antofagasta",
+  descripcion:"Capital regional: Antogafasta",
+  imagen:"./assets/img/antofa.jpg"
+};
+let r4 = {
+  nombre:"Atacama",
+  descripcion:"Capital regional: Copiapó",
+  imagen:"./assets/img/atacama.jpg"
+}
+
+let r5 = {
+    nombre:"Coquimbo",
+    descripcion:"Capital regional: La Serena",
+    imagen:"./assets/img/coquimbo.jpeg"
+  }
+
+  let r6 = {
+    nombre:"Valparaíso",
+    descripcion:"Capital regional: Valparaíso",
+    imagen:"./assets/img/valpo.jpg"
+  }
+
+  let r7 = {
+    nombre:"Metropolitana",
+    descripcion:"Capital regional: Santiago",
+    imagen:"./assets/img/stgo.jpg"
+  }
+
+  let r8 = {
+    nombre:"O'Higgins",
+    descripcion:"Capital regional: Rancagua",
+    imagen:"./assets/img/ohiggins.jpg"
+  }
+
+  let r9 = {
+    nombre:"Maule",
+    descripcion:"Capital regional: Talca",
+    imagen:"./assets/img/maule.jpg"
+  }
+
+  let r10 = {
+    nombre:"Ñuble",
+    descripcion:"Capital regional: Chillán",
+    imagen:"./assets/img/nuble.jpg"
+  }
+
+  let r11 = {
+    nombre:"Biobío",
+    descripcion:"Capital regional: Concepción",
+    imagen:"./assets/img/conce.jpg"
+  }
+
+  let r12 = {
+    nombre:"Araucanía",
+    descripcion:"Capital regional: Temuco",
+    imagen:"./assets/img/araucania.jpg"
+  }
+
+  let r13 = {
+    nombre:"Los Ríos",
+    descripcion:"Capital regional: Valdivia",
+    imagen:"./assets/img/rios.jpg"
+  }
+
+  let r14 = {
+    nombre:"Los Lagos",
+    descripcion:"Capital regional: Puerto Montt",
+    imagen:"./assets/img/lagos.jpg"
+  }
+
+  let r15 = {
+    nombre:"Aysén",
+    descripcion:"Capital regional: Coyhaique",
+    imagen:"./assets/img/aysen.jpg"
+  }
+
+  let r16 = {
+    nombre:"Magallanes",
+    descripcion:"Capital regional: Punta Arenas",
+    imagen:"./assets/img/magallanes.jpg"
+  }
+
+let regiones = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16];
+
+function actualizarDatos(){
+  console.log("ACTUALIZDO");
+}
+function cerrarForm(){
+  formActualizar.setAttribute('data-visible', false);
+}
+regionesDivs.forEach((element, i) => {
+  element.addEventListener('click', (event) =>{
+    formActualizar.setAttribute('data-visible', true);
+  });
+
+  element.addEventListener('mouseover',()=>{
+    carta.style.display = 'flex';
+    carta.innerHTML = 
+    '<img class="carta-img" src="'+ regiones[i].imagen+'"></img>'+
+    '<div class="carta-title">'+ regiones[i].nombre+'</div>' + 
+    '<div class="carta-text">'+ regiones[i].descripcion +'</div>';
+  });
+  element.addEventListener('mouseout',()=>{
+    carta.style.display = 'none';
+  });
+
 });
 
-const r2 = document.getElementById('antofagasta');
-const tarjetaAntofagasta = document.getElementById('tarjeta-antofagasta');
-r2.addEventListener('mouseenter', function(){
-    tarjetaAntofagasta.style.display = 'flex';
-});
-r2.addEventListener('mouseleave', function(){
-    tarjetaAntofagasta.style.display = 'none';
-});
-
-const r1 = document.getElementById('tarapaca');
-const tarjetaTarapaca = document.getElementById('tarjeta-tarapaca');
-r1.addEventListener('mouseenter', function(){
-    tarjetaTarapaca.style.display = 'flex';
-});
-r1.addEventListener('mouseleave', function(){
-    tarjetaTarapaca.style.display = 'none';
-});
-
-const r3 = document.getElementById('atacama');
-const tarjetaAtacama = document.getElementById('tarjeta-atacama');
-r3.addEventListener('mouseenter', function(){
-    tarjetaAtacama.style.display = 'flex';
-});
-r3.addEventListener('mouseleave', function(){
-    tarjetaAtacama.style.display = 'none';
-});
-
-const r4 = document.getElementById('coquimbo');
-const tarjetaCoquimbo = document.getElementById('tarjeta-coquimbo');
-r4.addEventListener('mouseenter', function(){
-    tarjetaCoquimbo.style.display = 'flex';
-});
-r4.addEventListener('mouseleave', function(){
-    tarjetaCoquimbo.style.display = 'none';
-});
-
-const r5 = document.getElementById('valparaiso');
-const tarjetaValparaiso = document.getElementById('tarjeta-valparaiso');
-r5.addEventListener('mouseenter', function(){
-    tarjetaValparaiso.style.display = 'flex';
-});
-r5.addEventListener('mouseleave', function(){
-    tarjetaValparaiso.style.display = 'none';
-});
-
-const r13 = document.getElementById('metropolitana');
-const tarjetaMetropolitana = document.getElementById('tarjeta-metropolitana');
-r13.addEventListener('mouseenter', function(){
-    tarjetaMetropolitana.style.display = 'flex';
-});
-r13.addEventListener('mouseleave', function(){
-    tarjetaMetropolitana.style.display = 'none';
-});
-
-const r6 = document.getElementById('ohiggins');
-const tarjetaOhiggins = document.getElementById('tarjeta-ohiggins');
-r6.addEventListener('mouseenter', function(){
-    tarjetaOhiggins.style.display = 'flex';
-});
-r6.addEventListener('mouseleave', function(){
-    tarjetaOhiggins.style.display = 'none';
-});
-
-const r7 = document.getElementById('maule');
-const tarjetaMaule = document.getElementById('tarjeta-maule');
-r7.addEventListener('mouseenter', function(){
-    tarjetaMaule.style.display = 'flex';
-});
-r7.addEventListener('mouseleave', function(){
-    tarjetaMaule.style.display = 'none';
-});
-
-const r8 = document.getElementById('biobio');
-const tarjetaBiobio = document.getElementById('tarjeta-biobio');
-r8.addEventListener('mouseenter', function(){
-    tarjetaBiobio.style.display = 'flex';
-});
-r8.addEventListener('mouseleave', function(){
-    tarjetaBiobio.style.display = 'none';
-});
-
-const r9 = document.getElementById('araucania');
-const tarjetaAraucania = document.getElementById('tarjeta-araucania');
-r9.addEventListener('mouseenter', function(){
-    tarjetaAraucania.style.display = 'flex';
-});
-r9.addEventListener('mouseleave', function(){
-    tarjetaAraucania.style.display = 'none';
-});
-
-const r14 = document.getElementById('rios');
-const tarjetaRios = document.getElementById('tarjeta-rios');
-r14.addEventListener('mouseenter', function(){
-    tarjetaRios.style.display = 'flex';
-});
-r14.addEventListener('mouseleave', function(){
-    tarjetaRios.style.display = 'none';
-});
-
-const r10 = document.getElementById('lagos');
-const tarjetaLagos = document.getElementById('tarjeta-lagos');
-r10.addEventListener('mouseenter', function(){
-    tarjetaLagos.style.display = 'flex';
-});
-r10.addEventListener('mouseleave', function(){
-    tarjetaLagos.style.display = 'none';
-});
-
-const r11 = document.getElementById('aisen');
-const tarjetaAisen = document.getElementById('tarjeta-aisen');
-r11.addEventListener('mouseenter', function(){
-    tarjetaAisen.style.display = 'flex';
-});
-r11.addEventListener('mouseleave', function(){
-    tarjetaAisen.style.display = 'none';
-});
-
-const r12 = document.getElementById('magallanes');
-const tarjetaMagallanes = document.getElementById('tarjeta-magallanes');
-r12.addEventListener('mouseenter', function(){
-    tarjetaMagallanes.style.display = 'flex';
-});
-r12.addEventListener('mouseleave', function(){
-    tarjetaMagallanes.style.display = 'none';
-});
-
-const r16 = document.getElementById('nuble');
-const tarjetaNuble = document.getElementById('tarjeta-nuble');
-r16.addEventListener('mouseenter', function(){
-    tarjetaNuble.style.display = 'flex';
-});
-r16.addEventListener('mouseleave', function(){
-    tarjetaNuble.style.display = 'none';
-});
+document.addEventListener('mousemove', (event)=>{
+  carta.style.left = event.pageX + 70 +'px';
+  carta.style.top = event.pageY + 'px';
+})
+btnCerrarForm.onclick = cerrarForm; 
+btnSubmit.onclick = actualizarDatos;
